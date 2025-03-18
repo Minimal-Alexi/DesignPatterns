283e9b5a84bdee88c0564692e807b98df86b0e44
Minimal-Alexi <alexpop@metropolia.fi>
2025-03-18 10:07:03 +0200

    Set up the MapAbstract class.

--- RPGMapGenerator/src/main/java/Map/Map.java
+++ RPGMapGenerator/src/main/java/Map/Map.java
@@ -1,5 +1,17 @@
 package Map;
 
-public abstract class Map {
+import Tiles.AbstractTile;
 
+public abstract class Map {
+    private final int mapSize = 5;
+    private AbstractTile[][] tiles = new AbstractTile[mapSize][mapSize];
+    public abstract AbstractTile createTile();
+    public void display(){
+        for(int i = 0; i < mapSize; i++){
+            for(int j = 0; j < mapSize; j++){
+                System.out.print(tiles[i][j].getDescriptorCharacter());
+            }
+            System.out.println();
+        }
+    }
 }
