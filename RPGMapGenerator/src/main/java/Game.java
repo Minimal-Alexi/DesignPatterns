01a7d0a7ab1549ed5bf888cc86cc9d1c3cac6535
Minimal-Alexi <alexpop@metropolia.fi>
2025-03-18 10:35:38 +0200

    Created game main.

--- RPGMapGenerator/src/main/java/Game.java
+++ RPGMapGenerator/src/main/java/Game.java
@@ -1,6 +1,43 @@
-public class Game {
-    private void createMap(){}
-    public static void main(String[] args) {
+import Map.*;
 
+import java.util.Scanner;
+
+public class Game {
+    private static Map gameMap;
+    private static void createMap(int mapOption)
+    {
+        if(mapOption == 1)
+        {
+            gameMap = new CityMap();
+        }
+        else
+        {
+            gameMap = new WildernessMap();
+        }
+        gameMap.display();
+    }
+    public static void main(String[] args) {
+        boolean run = true;
+        while (run)
+        {
+            System.out.println("Welcome to the Map Game");
+            System.out.println("Please select a map option");
+            System.out.println("1. Create a City map");
+            System.out.println("2. Create a Wilderness map");
+            System.out.println("3. Exit");
+            Scanner scanner = new Scanner(System.in);
+            int mapOption = scanner.nextInt();
+            switch(mapOption)
+            {
+                case 3:
+                {
+                    run = false;
+                }
+                default:
+                {
+                    createMap(mapOption);
+                }
+            }
+        }
     }
 }
