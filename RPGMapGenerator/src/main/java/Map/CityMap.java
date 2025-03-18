9a423b9ef3060015f67fcf15fca787c837aa0f2e
Minimal-Alexi <alexpop@metropolia.fi>
2025-03-18 10:26:12 +0200

    Created CityMap.

--- RPGMapGenerator/src/main/java/Map/CityMap.java
+++ RPGMapGenerator/src/main/java/Map/CityMap.java
@@ -1,11 +1,32 @@
 package Map;
 
 import Tiles.AbstractTile;
+import Tiles.BuildingTile;
+import Tiles.ForestTile;
+import Tiles.RoadTile;
+
+import java.util.Random;
 
 public class CityMap extends Map {
 
     @Override
     protected AbstractTile createTile(){
-
+        Random rand = new Random();
+        switch(rand.nextInt(3))
+        {
+            case 0:
+            {
+                return new RoadTile();
+            }
+            case 1:
+            {
+                return new ForestTile();
+            }
+            case 2:
+            {
+                return new BuildingTile();
+            }
+        }
+        return null;
     }
 }
