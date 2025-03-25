8b5feac439f3bcd09548b89b4e119c7094edbe3e
Minimal-Alexi <alexpop@metropolia.fi>
2025-03-25 13:50:31 +0200

    Created main.

--- Organization-Structure/src/main/java/Main.java
+++ Organization-Structure/src/main/java/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
+        // Departments
+        Department initialNode = new Department("Minimal Transport Inc."), itDepartment = new Department("IT Department"),
+                busDepartment = new Department("Bus Department"),customerSupport = new Department("Customer Support");
+
+        // Employees
+        Employee CEO = new Employee("Minerva",1000000), itEngineer = new Employee("John",150000),
+                busDriver = new Employee("George",40000),busDriver2 = new Employee("John",55000),
+                customerTechnician = new Employee("Sophie",70000);
+
+        // Creating company structure
+        initialNode.addComponent(itDepartment);
+        initialNode.addComponent(busDepartment);
+        initialNode.addComponent(CEO);
+
+        itDepartment.addComponent(itEngineer);
+        itDepartment.addComponent(customerSupport);
+
+        customerSupport.addComponent(customerTechnician);
+
+        busDepartment.addComponent(busDriver);
+        busDepartment.addComponent(busDriver2);
+
+        System.out.println("Total company expenses should be 1,315,000$: " + initialNode.getTotalSalary());
+        System.out.println("It department expenses should be 220,000$:" + itDepartment.getTotalSalary());
+        System.out.println("Customer support expenses should be 70,000$:" + customerSupport.getTotalSalary());
+        System.out.println("Bus driver expenses should be 95,000$: " + busDepartment.getTotalSalary());
 
     }
 }
