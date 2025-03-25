950e5fe0c089f4dc45961f66586450f02f2806a2
Minimal-Alexi <alexpop@metropolia.fi>
2025-03-25 15:18:54 +0200

    Created observer.

--- WeatherObserver/src/main/java/WeatherObserver.java
+++ WeatherObserver/src/main/java/WeatherObserver.java
@@ -1,7 +1,12 @@
 public class WeatherObserver implements Observer {
-
+    private int id;
+    private final WeatherStation station;
+    public WeatherObserver(int id,WeatherStation station) {
+        this.id = id;
+        this.station = station;
+    }
     public void update(){
-
+        System.out.println("Observer " + id + ": " + station.getTemperature() +" , the weather is currently: " + station.getWeather());
     }
 
 }
