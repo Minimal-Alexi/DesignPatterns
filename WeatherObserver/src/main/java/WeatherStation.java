c984240152c8f25da61ff7be5b6c85a22c83abeb
Minimal-Alexi <alexpop@metropolia.fi>
2025-03-25 15:11:37 +0200

    Setting up weather station.

--- WeatherObserver/src/main/java/WeatherStation.java
+++ WeatherObserver/src/main/java/WeatherStation.java
@@ -1,3 +1,50 @@
-public class WeatherStation {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+public class WeatherStation {
+    private List<Observer> observers;
+    private Random rand = new Random();
+    private float temperature;
+    private String weather;
+    public WeatherStation() {
+        observers = new ArrayList<Observer>();
+        temperature = rand.nextFloat(-20,20);
+        generateWeather();
+    }
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+    public void notifyObservers() {
+        for (Observer observer : observers) {
+            observer.update();
+        }
+    }
+    public void run()
+    {
+
+    }
+    private void generateWeather() {
+        int weatherValue = rand.nextInt(3);
+        switch (weatherValue) {
+            case 0:
+            {
+                weather = "Sunny";
+                break;
+            }
+            case 1:
+            {
+                weather = "Cloudy";
+                break;
+            }
+            case 2:
+            {
+                weather = "Rainy";
+                break;
+            }
+        }
+    }
 }
