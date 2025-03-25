c8a73ef3ed6da17632b8c8cb0f748493b23cf9e3
Minimal-Alexi <alexpop@metropolia.fi>
2025-03-25 13:59:15 +0200

    Tested a bit more in main. Should've worked anyways. Obviously

--- Organization-Structure/src/main/java/Main.java
+++ Organization-Structure/src/main/java/Main.java
@@ -22,10 +22,21 @@ public class Main {
         busDepartment.addComponent(busDriver);
         busDepartment.addComponent(busDriver2);
 
+        // Testing cost calculator
         System.out.println("Total company expenses should be 1,315,000$: " + initialNode.getTotalSalary());
         System.out.println("It department expenses should be 220,000$:" + itDepartment.getTotalSalary());
         System.out.println("Customer support expenses should be 70,000$:" + customerSupport.getTotalSalary());
         System.out.println("Bus driver expenses should be 95,000$: " + busDepartment.getTotalSalary());
+
+        System.out.println();
+
+        // Let's fire a bus driver, clearly that wouldn't hurt the bottom line
+        busDepartment.removeComponent(busDriver2);
+        System.out.println("Bus driver expenses should be 40,000$: " + busDepartment.getTotalSalary());
+        // Quick, re-hire him, our stock markets are plummeting
+        busDepartment.addComponent(busDriver2);
+
+        // Printing timeeeeeee
 
     }
 }
