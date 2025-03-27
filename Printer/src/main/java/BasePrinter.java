8536be7e995d6f630d42c2e208631eb42ca3a97e
Minimal-Alexi <alexpop@metropolia.fi>
2025-03-27 12:26:53 +0200

    Creating skeleton.

---
+++ Printer/src/main/java/BasePrinter.java
@@ -0,0 +1,8 @@
+public class BasePrinter implements Printer {
+    public BasePrinter() {
+
+    }
+    public void print(String text) {
+        System.out.println(text);
+    }
+}
---
+++ Printer/src/main/java/PrinterDecorator.java
@@ -0,0 +1,10 @@
+public class PrinterDecorator implements Printer {
+    private Printer printer;
+    public PrinterDecorator(Printer printer) {
+        this.printer = printer;
+    }
+    @Override
+    public void print(String text) {
+        this.printer.print(text);
+    }
+}
