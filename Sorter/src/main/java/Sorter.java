a3648688847ee2239cfab769eb68e3465c54f579
Minimal-Alexi <alexpop@metropolia.fi>
2025-04-03 10:30:53 +0300

    Fixed bug and made better display.

--- Sorter/src/main/java/Sorter.java
+++ Sorter/src/main/java/Sorter.java
@@ -14,18 +14,18 @@ public class Sorter {
         long endTime = System.currentTimeMillis();
         long elapsedTime = endTime - startTime;
         if(sortCheck(sortedList)) {
-            System.out.println("List of " + sortedList.size() + " elements sorted in " + elapsedTime + "ms using the: " + strategy.getName());
+            System.out.println("List of " + sortedList.size() + " elements sorted in " + elapsedTime + "ms using the: " + strategy.getName() + "\n");
         }
         else {
-            System.out.println("List of " + sortedList.size() + " elements failed sorting in " + elapsedTime + "ms using the: " + strategy.getName());
+            System.out.println("List of " + sortedList.size() + " elements failed sorting in " + elapsedTime + "ms using the: " + strategy.getName() + "\n");
         }
     }
     public void setStrategy(SorterStrategy strategy){
         this.strategy = strategy;
     }
     private boolean sortCheck(ArrayList<Integer> list){
-        for(int i = 0; i<list.size(); i++){
-            if(list.get(i)<list.get(i+1)){
+        for(int i = 0; i<list.size() - 1; i++){
+            if(list.get(i)>list.get(i+1)){
                 return false;
             }
         }
