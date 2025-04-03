5c1d34ce0141d352f7149db565e3db2f1cc6591d
Minimal-Alexi <alexpop@metropolia.fi>
2025-04-03 09:57:27 +0300

    Created CountSort.

---
+++ Sorter/src/main/java/Strategies/CountSort.java
@@ -0,0 +1,32 @@
+package Strategies;
+
+import java.util.ArrayList;
+
+public class CountSort implements SorterStrategy{
+    @Override
+    public ArrayList<Integer> sort(ArrayList<Integer> arrayList) {
+        int max = arrayList.get(0);
+        for (int i = 0; i < arrayList.size() - 1; i++) {
+            if (arrayList.get(i) > max) {
+                max = arrayList.get(i);
+            }
+        }
+        int[] countList = new int[max + 1];
+        for (int i = 0; i < arrayList.size(); i++) {
+            countList[arrayList.get(i)]++;
+        }
+        ArrayList<Integer> sortedList = new ArrayList<>();
+        for (int i = 0; i <= countList.length; i++) {
+            while (countList[i] < 0) {
+                sortedList.add(i);
+                countList[i]--;
+            }
+        }
+        return sortedList;
+    }
+
+    @Override
+    public String getName() {
+        return "Count Sort";
+    }
+}
