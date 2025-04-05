7a979a18ff22828ec75e03c7ce24c9a6c16f2bef
Minimal-Alexi <alexpop@metropolia.fi>
2025-04-05 14:53:30 +0300

    Preparing code.

--- GameFramework/src/main/java/Player.java
+++ GameFramework/src/main/java/Player.java
@@ -1,4 +1,28 @@
 public class Player {
     private String name;
     private int resourceSteel,resourceOil,resourceRubber;
+    public Player(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getResourceSteel() {
+        return resourceSteel;
+    }
+    public int getResourceOil() {
+        return resourceOil;
+    }
+    public int getResourceRubber() {
+        return resourceRubber;
+    }
+    public void changeResourceSteel(int newSteel) {
+        resourceSteel += newSteel;
+    }
+    public void changeResourceOil(int newOil) {
+        resourceOil += newOil;
+    }
+    public void changeResourceRubber(int newRubber) {
+        resourceRubber += newRubber;
+    }
 }
--- GameFramework/src/main/java/Tile.java
+++ GameFramework/src/main/java/Tile.java
@@ -1,8 +1,64 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Tile {
     private Player player;
     private TileType type;
     private ArrayList<Units> units;
-    private int incomeSteel,incomeOil,incomeRubber;
+    private int incomeSteel,incomeOil,incomeRubber, defenderAdvantage;
+    public Tile(TileType type, Player player, int incomeSteel, int incomeOil, int incomeRubber) {
+        this.type = type;
+        this.player = player;
+        this.incomeSteel = incomeSteel;
+        this.incomeOil = incomeOil;
+        this.incomeRubber = incomeRubber;
+        switch (type) {
+            case land -> defenderAdvantage = 1;
+            case water -> defenderAdvantage = 0;
+            case mountain -> defenderAdvantage = 2;
+        }
+    }
+    public void handleBattle(ArrayList<Units> enemyUnits) {
+        while(!enemyUnits.isEmpty() && !units.isEmpty()) {
+            int defenderStrength = strengthCalculator(units) + defenderAdvantage;
+            int attackerStrength = strengthCalculator(enemyUnits);
+            Random rand = new Random();
+            int defenderScore = rand.nextInt(defenderStrength);
+            int attackerScore = rand.nextInt(attackerStrength);
+            if(defenderScore > attackerScore) {
+                enemyUnits.remove(0);
+            }
+            else {
+                if(attackerScore == defenderScore) {
+                    enemyUnits.remove(0);
+                    units.remove(0);
+                }
+                else {
+                    units.remove(0);
+                }
+            }
+        }
+        if(units.isEmpty()) {
+            changeOwnership(enemyUnits.get(0).getPlayer());
+        }
+    }
+    public void changeOwnership(Player player) {
+        this.player.changeResourceOil(-incomeOil);
+        this.player.changeResourceSteel(-incomeSteel);
+        this.player.changeResourceRubber(-incomeRubber);
+        this.player = player;
+        sendResources();
+    }
+    public void sendResources(){
+        this.player.changeResourceSteel(incomeSteel);
+        this.player.changeResourceOil(incomeOil);
+        this.player.changeResourceRubber(incomeRubber);
+    }
+    private int strengthCalculator(ArrayList<Units> units){
+        int strength = 0;
+        for(Units unit: units){
+            strength += unit.getStrength();
+        }
+        return strength;
+    }
 }
