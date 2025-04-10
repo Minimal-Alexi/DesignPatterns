89b48b29aaa43e5109fc99ea218a909e7d99e340
Minimal-Alexi <alexpop@metropolia.fi>
2025-04-10 10:15:41 +0300

    Created ColorBox object.

--- GUIHistory/src/main/java/Model/ColorBox.java
+++ GUIHistory/src/main/java/Model/ColorBox.java
@@ -1,5 +1,17 @@
 package Model;
 
-public class ColorBox {
+import javafx.scene.paint.Color;
 
+public class ColorBox {
+    private final Color[] color = {Color.RED,Color.YELLOW,Color.BLUE};
+    private int colorIndex;
+    public ColorBox() {
+        colorIndex = 0;
+    }
+    public void setColorIndex(int colorIndex) {
+        this.colorIndex = colorIndex;
+    }
+    public Color getColor() {
+        return color[colorIndex];
+    }
 }
