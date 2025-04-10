96f1cea31804a0e13c227fca88c3c925ff457852
Minimal-Alexi <alexpop@metropolia.fi>
2025-04-10 11:21:43 +0300

    Added debug text.

--- GUIHistory/src/main/java/Model/ColorBox.java
+++ GUIHistory/src/main/java/Model/ColorBox.java
@@ -3,18 +3,32 @@ package Model;
 import javafx.scene.paint.Color;
 
 public class ColorBox {
-    private final Color[] color = {Color.RED,Color.YELLOW,Color.BLUE};
+    private final Color[] color = {Color.RED, Color.YELLOW, Color.BLUE};
     private int colorIndex;
+
     public ColorBox() {
         colorIndex = 0;
+        System.out.println("ColorBox initialized with default color index = 0 (Color = " + color[colorIndex] + ")");
     }
+
     public void setColorIndex(int colorIndex) {
-        this.colorIndex = colorIndex;
+        if (colorIndex >= 0 && colorIndex < color.length) {
+            System.out.println("Setting ColorBox color index from " + this.colorIndex + " to " + colorIndex +
+                    " (Color = " + color[colorIndex] + ")");
+            this.colorIndex = colorIndex;
+        } else {
+            System.out.println("Invalid color index: " + colorIndex);
+        }
     }
+
     public Color getColor() {
-        return color[colorIndex];
+        Color currentColor = color[colorIndex];
+        System.out.println("Retrieving color at index " + colorIndex + ": " + currentColor);
+        return currentColor;
     }
+
     public int getColorIndex() {
+        System.out.println("Retrieving current color index: " + colorIndex);
         return colorIndex;
     }
 }
--- GUIHistory/src/main/java/Model/State.java
+++ GUIHistory/src/main/java/Model/State.java
@@ -10,36 +10,57 @@ public class State {
         colorBoxes = new ColorBox[3];
         for (int i = 0; i < colorBoxes.length; i++) {
             colorBoxes[i] = new ColorBox();
+            System.out.println("Initialized ColorBox at index " + i);
         }
         isSelected = false;
+        System.out.println("State initialized with isSelected = false");
     }
-    public void setOptionChoice(int option,int choice) {
-        if(option >= 1 && option <= 3) {
+
+    public void setOptionChoice(int option, int choice) {
+        if (option >= 1 && option <= 3) {
+            System.out.println("Setting color index for option " + option + " to " + choice);
             colorBoxes[option - 1].setColorIndex(choice);
+        } else {
+            System.out.println("Invalid option index: " + option);
         }
     }
-    public Color getOption(int option){
-        if(option >= 1 && option <= 3) {
-            return colorBoxes[option - 1].getColor();
+
+    public Color getOption(int option) {
+        if (option >= 1 && option <= 3) {
+            Color color = colorBoxes[option - 1].getColor();
+            System.out.println("Retrieved color for option " + option + ": " + color);
+            return color;
         }
+        System.out.println("Invalid option index: " + option);
         return null;
     }
+
     public boolean isSelected() {
+        System.out.println("isSelected queried: " + isSelected);
         return isSelected;
     }
+
     public void setSelected(boolean selected) {
-        isSelected = selected;
+        this.isSelected = selected;
+        System.out.println("isSelected set to: " + selected);
     }
+
     public ColorBox[] getColorBoxes() {
+        System.out.println("getColorBoxes called");
         return colorBoxes;
     }
+
     public IMemento createMemento() {
+        System.out.println("Creating memento");
         return new Memento(this);
     }
+
     public void restoreState(IMemento memento) {
+        System.out.println("Restoring state from memento");
         Memento selectedMemento = (Memento) memento;
         State selectedState = selectedMemento.getState();
         this.colorBoxes = selectedState.getColorBoxes();
         this.isSelected = selectedState.isSelected();
+        System.out.println("State restored: isSelected = " + this.isSelected);
     }
 }
