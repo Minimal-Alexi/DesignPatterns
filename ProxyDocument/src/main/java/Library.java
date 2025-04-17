ddfb8f6fde969bebbbdc470e0c96071ca657cc11
Minimal-Alexi <alexpop@metropolia.fi>
2025-04-17 11:08:10 +0300

    Modified factory methods.

--- ProxyDocument/src/main/java/Library.java
+++ ProxyDocument/src/main/java/Library.java
@@ -1,5 +1,6 @@
 import Document.*;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 
 public class Library {
@@ -16,7 +17,11 @@ public class Library {
     public Document getDocument(int index) {
         return documentLibrary.get(index);
     }
-    public DocumentProxy secureDocument(DocumentReal realDocument) {
+    public DocumentReal createUnsecureDocument(int UID, String content){
+        return new DocumentReal(content,new Timestamp(System.currentTimeMillis()),UID);
+    }
+    public DocumentProxy createSecureDocument(int UID, String content) {
+        DocumentReal realDocument = createUnsecureDocument(UID, content);
         return new DocumentProxy(realDocument);
     }
 }
