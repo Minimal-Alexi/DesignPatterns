12032a1efe74a374ab5d277c6cc3287519fd6adf
Minimal-Alexi <alexpop@metropolia.fi>
2025-04-17 10:24:36 +0300

    Finished proxy docs.

--- ProxyDocument/src/main/java/DocumentProxy.java
+++ ProxyDocument/src/main/java/DocumentProxy.java
@@ -1,17 +1,20 @@
+import java.nio.file.AccessDeniedException;
 import java.sql.Timestamp;
 
 public class DocumentProxy implements Document{
     private final DocumentReal documentReal;
     private final AccessControlService accessControlService = AccessControlService.getInstance();
     public DocumentProxy(int UID, String content) {
-        documentReal = new DocumentReal();
+        documentReal = new DocumentReal(content,new Timestamp(System.currentTimeMillis()),UID);
     }
     @Override
-    public String getContent(User user) {
+    public String getContent(User user) throws AccessDeniedException {
         if(accessControlService.isAllowed(documentReal.getUID(),user)){
-
+            return documentReal.getContent(user);
         }
-        return documentReal.getContent(user);
+        else{
+            throw new AccessDeniedException("Access denied to user: " + user.getUsername() + " for document UID: " + documentReal.getUID());
+        }
     }
 
     @Override
--- ProxyDocument/src/main/java/DocumentReal.java
+++ ProxyDocument/src/main/java/DocumentReal.java
@@ -4,6 +4,11 @@ public class DocumentReal implements Document {
     private String content;
     private Timestamp creationDate;
     private int id;
+    public DocumentReal(String content, Timestamp creationDate, int id) {
+        this.content = content;
+        this.creationDate = creationDate;
+        this.id = id;
+    }
     @Override
     public String getContent(User user) {
         return content;
