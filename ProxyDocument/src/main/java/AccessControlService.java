1133ddbbcee560b5be3de94a16853467e2041d9f
Minimal-Alexi <alexpop@metropolia.fi>
2025-04-17 10:05:49 +0300

    Set up AccessControlService.

--- ProxyDocument/src/main/java/AccessControlService.java
+++ ProxyDocument/src/main/java/AccessControlService.java
@@ -1,4 +1,14 @@
 public class AccessControlService {
+    private static AccessControlService instance;
+    private AccessControlService() {
+
+    }
+    public static AccessControlService getInstance() {
+        if (instance == null) {
+            instance = new AccessControlService();
+        }
+        return instance;
+    }
     public boolean isAllowed(User user){
         return true;
     }
