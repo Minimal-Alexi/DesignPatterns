3be85097418eb52207e0ce33d19fabc9d34d1b87
Minimal-Alexi <alexpop@metropolia.fi>
2025-04-17 10:02:29 +0300

    Setting up proxy and real.

--- ProxyDocument/src/main/java/DocumentProxy.java
+++ ProxyDocument/src/main/java/DocumentProxy.java
@@ -1,19 +1,21 @@
 import java.sql.Timestamp;
 
 public class DocumentProxy implements Document{
-
+    private String content;
+    private Timestamp creationDate;
+    private int id;
     @Override
     public String getContent() {
-        return "";
+        return content;
     }
 
     @Override
     public int getUID() {
-        return 0;
+        return id;
     }
 
     @Override
     public Timestamp getCreationDate() {
-        return null;
+        return creationDate;
     }
 }
--- ProxyDocument/src/main/java/DocumentReal.java
+++ ProxyDocument/src/main/java/DocumentReal.java
@@ -1,18 +1,21 @@
 import java.sql.Timestamp;
 
 public class DocumentReal implements Document {
+    private String content;
+    private Timestamp creationDate;
+    private int id;
     @Override
     public String getContent() {
-        return "";
+        return content;
     }
 
     @Override
     public int getUID() {
-        return 0;
+        return id;
     }
 
     @Override
     public Timestamp getCreationDate() {
-        return null;
+        return creationDate;
     }
 }
