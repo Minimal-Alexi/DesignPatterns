b662db58f171c25394f2d5cb62dc286306ed1295
Minimal-Alexi <alexpop@metropolia.fi>
2025-04-22 14:09:49 +0300

    Differentiated the builders.

--- ComputerBuilder/src/main/java/GamingComputerBuilder.java
+++ ComputerBuilder/src/main/java/GamingComputerBuilder.java
@@ -4,36 +4,37 @@ public class GamingComputerBuilder implements ComputerBuilderI{
     public GamingComputerBuilder(int quality){
         computer = new Computer();
     }
+    @Override
     public void setQuality(int quality){
         this.quality = quality;
     }
     @Override
     public void buildCPU() {
-
+        computer.addComponent(new Component("RGB CPU",quality));
     }
 
     @Override
     public void buildRAM() {
-
+        computer.addComponent(new Component("32 GB RAM",quality));
     }
 
     @Override
     public void buildHardDrive() {
-
+        computer.addComponent(new Component("2 TB Pirate HDD",quality));
     }
 
     @Override
     public void buildGPU() {
-
+        computer.addComponent(new Component("RTX 10090", quality));
     }
 
     @Override
     public void buildOS() {
-
+        computer.addComponent(new Component("Arch-Linux",quality));
     }
 
     @Override
     public Computer getComputer() {
-        return null;
+        return computer;
     }
 }
--- ComputerBuilder/src/main/java/OfficeComputerBuilder.java
+++ ComputerBuilder/src/main/java/OfficeComputerBuilder.java
@@ -4,36 +4,37 @@ public class OfficeComputerBuilder implements ComputerBuilderI{
     public OfficeComputerBuilder() {
         computer = new Computer();
     }
+    @Override
     public void setQuality(int quality){
         this.quality = quality;
     }
     @Override
     public void buildCPU() {
-
+        computer.addComponent(new Component("Serious CPU",quality));
     }
 
     @Override
     public void buildRAM() {
-
+        computer.addComponent(new Component("8 GB RAM",quality));
     }
 
     @Override
     public void buildHardDrive() {
-
+        computer.addComponent(new Component("512 GB HDD",quality));
     }
 
     @Override
     public void buildGPU() {
-
+        computer.addComponent(new Component("GTX 1080", quality));
     }
 
     @Override
     public void buildOS() {
-
+        computer.addComponent(new Component("AppleOS",quality));
     }
 
     @Override
     public Computer getComputer() {
-        return null;
+        return computer;
     }
 }
