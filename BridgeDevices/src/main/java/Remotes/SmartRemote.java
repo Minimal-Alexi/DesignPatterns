20d672dfe7fb66003e870ae247b1d9777097a48e
Minimal-Alexi <alexpop@metropolia.fi>
2025-04-28 11:35:51 +0300

    Created SmartRemote.

---
+++ BridgeDevices/src/main/java/Remotes/SmartRemote.java
@@ -0,0 +1,34 @@
+package Remotes;
+
+import Devices.DeviceI;
+
+public class SmartRemote extends AdvancedRemote{
+
+    public SmartRemote(DeviceI device) {
+        super(device);
+    }
+    public void voiceControl(String command, int value){
+        switch(command){
+            case "mute":
+            {
+                mute();
+                break;
+            }
+            case "unmute":{
+                device.setVolume(0);
+                break;
+            }
+            case "change channel":{
+                device.setChannel(value);
+                break;
+            }
+            case "change volume":{
+                device.setVolume(value);
+                break;
+            }
+            default:{
+                System.out.println("Unknown command");
+            }
+        }
+    }
+}
