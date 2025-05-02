582678d21ad35ca572de1185a7b381034b274e07
Minimal-Alexi <alexpop@metropolia.fi>
2025-05-02 10:43:39 +0300

    Prepared main loop.

--- BookSystem/src/main/java/Main.java
+++ BookSystem/src/main/java/Main.java
@@ -9,5 +9,20 @@ public class Main {
         farmerRecommendation.addRecommendedBook(book3);
 
         fashionRecommendation.addRecommendedBook(book2);
+        System.out.println("Initial lists.");
+        System.out.println(farmerRecommendation);
+        System.out.println(fashionRecommendation);
+
+        Recommendation fashionableFarmerRecommendation = fashionRecommendation.clone();
+        fashionableFarmerRecommendation.setTargetAudience("Fashionable farmer");
+        System.out.println("Let's copy the fashion recommendation and make a fashionable farmer recommendation.");
+        System.out.println(fashionableFarmerRecommendation);
+        System.out.println(fashionRecommendation);
+
+        System.out.println("Let's differentiate the two recommendations more.");
+        fashionableFarmerRecommendation.addRecommendedBook(book3);
+        book2.setAuthor("Minerva-Alexia");
+        System.out.println(fashionableFarmerRecommendation);
+        System.out.println(fashionRecommendation);
     }
 }
