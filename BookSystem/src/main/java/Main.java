86927f0bea87768f32fe09556c1f15edaab36c3b
Minimal-Alexi <alexpop@metropolia.fi>
2025-05-02 10:35:18 +0300

    Preparing main.

--- BookSystem/src/main/java/Main.java
+++ BookSystem/src/main/java/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+    public static void main(String[] args) {
+        Book book1 = new Book("How to farm bananas","John Banana"), book2 = new Book("Women's Fashion","Minerva A"),
+                book3 = new Book("Farmers' fashion","John Fashion");
+        Recommendation farmerRecommendation = new Recommendation("Banana farmers"),
+                fashionRecommendation = new Recommendation("Fashionistas");
+
+        farmerRecommendation.addRecommendedBook(book1);
+        farmerRecommendation.addRecommendedBook(book3);
+
+        fashionRecommendation.addRecommendedBook(book2);
+    }
 }
