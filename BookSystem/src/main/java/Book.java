03b1b6c307d4367e3af763048ce5a2ba9ffc3c0c
Minimal-Alexi <alexpop@metropolia.fi>
2025-05-02 10:28:12 +0300

    Initial commit.

---
+++ BookSystem/src/main/java/Book.java
@@ -0,0 +1,17 @@
+public class Book implements Prototype{
+    private String title,author;
+    public Book(String title, String author){
+        this.title = title;
+        this.author = author;
+    }
+    @Override
+    public Book clone(){
+        return new Book(title,author);
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getAuthor(){
+        return author;
+    }
+}
---
+++ BookSystem/src/main/java/Recommendation.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Recommendation implements Prototype{
+    private String targetAudience;
+    private ArrayList<Book> recommendedBooks;
+    public Recommendation(String targetAudience, ArrayList<Book> recommendedBooks) {
+        this.targetAudience = targetAudience;
+        this.recommendedBooks = recommendedBooks;
+    }
+    public Recommendation clone() {
+        try{
+            Recommendation clonedRecommendation = (Recommendation) super.clone();
+            clonedRecommendation.targetAudience = this.targetAudience;
+            clonedRecommendation.recommendedBooks = new ArrayList<>();
+            for(Book book : this.recommendedBooks){
+                clonedRecommendation.addRecommendedBook(book.clone());
+            }
+            return clonedRecommendation;
+        }catch(CloneNotSupportedException e){
+            throw new AssertionError();
+        }
+    }
+    public String getTargetAudience() {
+        return targetAudience;
+    }
+    public void setTargetAudience(String targetAudience) {
+        this.targetAudience = targetAudience;
+    }
+    public ArrayList<Book> getRecommendedBooks() {
+        return recommendedBooks;
+    }
+    public void addRecommendedBook(Book recommendedBook) {
+        this.recommendedBooks.add(recommendedBook);
+    }
+    public void removeRecommendedBook(Book recommendedBook) {
+        this.recommendedBooks.remove(recommendedBook);
+    }
+}
