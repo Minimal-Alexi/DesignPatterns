25bcbc3be4ec479f5cb584c8c413ff472bb39d2d
Minimal-Alexi <alexpop@metropolia.fi>
2025-05-04 15:03:03 +0300

    Created invoker class.

---
+++ PixelArt/src/main/java/controller/CommandInvoker.java
@@ -0,0 +1,36 @@
+package controller;
+
+import commands.Command;
+
+public class CommandInvoker {
+    private Command generateCodeCommand, moveDownCommand,moveUpCommand,moveRightCommand,moveLeftCommand,
+            togglePixelCommand;
+    public CommandInvoker(Command generateCodeCommand,Command moveDownCommand,Command moveUpCommand,
+                          Command moveRightCommand,Command moveLeftCommand,
+                          Command togglePixelCommand) {
+        this.generateCodeCommand = generateCodeCommand;
+        this.moveDownCommand = moveDownCommand;
+        this.moveUpCommand = moveUpCommand;
+        this.moveRightCommand = moveRightCommand;
+        this.moveLeftCommand = moveLeftCommand;
+        this.togglePixelCommand = togglePixelCommand;
+    }
+    public void generateCode(){
+        generateCodeCommand.execute();
+    }
+    public void moveDown(){
+        moveDownCommand.execute();
+    }
+    public void moveUp(){
+        moveUpCommand.execute();
+    }
+    public void moveRight(){
+        moveRightCommand.execute();
+    }
+    public void moveLeft(){
+        moveLeftCommand.execute();
+    }
+    public void togglePixel(){
+        togglePixelCommand.execute();
+    }
+}
--- PixelArt/src/main/java/controller/PixelArtController.java
+++ PixelArt/src/main/java/controller/PixelArtController.java
@@ -3,14 +3,19 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import model.CanvasStatus;
 
 public class PixelArtController {
     private final static int pixelSize = 16;
+    private CanvasStatus canvasStatus;
+    @FXML
+    private VBox vBox;
     @FXML
     private Canvas canvasPixelArt;
     @FXML
     private Button buttonGenerateCode;
     public void initialize(){
-
+        
     }
 }
