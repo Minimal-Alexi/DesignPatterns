b4033b5f0e51effc96c88564149c6fd579b9125d
Minimal-Alexi <alexpop@metropolia.fi>
2025-05-04 15:16:35 +0300

    Temp commit.

--- PixelArt/src/main/java/controller/PixelArtController.java
+++ PixelArt/src/main/java/controller/PixelArtController.java
@@ -1,7 +1,9 @@
 package controller;
 
+import commands.*;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import model.CanvasStatus;
@@ -9,6 +11,8 @@ import model.CanvasStatus;
 public class PixelArtController {
     private final static int pixelSize = 16;
     private CanvasStatus canvasStatus;
+    private CommandInvoker commandInvoker;
+    private GraphicsContext graphicsContext;
     @FXML
     private VBox vBox;
     @FXML
@@ -16,6 +20,30 @@ public class PixelArtController {
     @FXML
     private Button buttonGenerateCode;
     public void initialize(){
-        
+        canvasStatus = new CanvasStatus();
+        refreshCanvas();
+        commandInitialization();
+
+    }
+    private void commandInitialization(){
+        GenerateCodeCommand generateCodeCommand = new GenerateCodeCommand(canvasStatus);
+        MoveCursorDownCommand moveCursorDownCommand = new MoveCursorDownCommand(canvasStatus);
+        MoveCursorLeftCommand moveCursorLeftCommand = new MoveCursorLeftCommand(canvasStatus);
+        MoveCursorRightCommand moveCursorRightCommand = new MoveCursorRightCommand(canvasStatus);
+        MoveCursorUpCommand moveCursorUpCommand = new MoveCursorUpCommand(canvasStatus);
+        TogglePixelCommand togglePixelCommand = new TogglePixelCommand(canvasStatus);
+        commandInvoker = new CommandInvoker(generateCodeCommand,moveCursorDownCommand,moveCursorUpCommand,
+                moveCursorRightCommand,moveCursorLeftCommand,togglePixelCommand);
+    }
+    private void refreshCanvas(){
+
+    }
+    private void codeGenerator(){
+        buttonGenerateCode.setOnAction(e -> {
+            commandInvoker.generateCode();
+        });
+    }
+    private void canvasUseInit(){
+
     }
 }
