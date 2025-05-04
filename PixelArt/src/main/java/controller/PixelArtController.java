117a611a53d445c6c1192d33ab7515117c4f6b06
Minimal-Alexi <alexpop@metropolia.fi>
2025-05-04 14:28:34 +0300

    Initialize JavaFX variables.

--- PixelArt/src/main/java/controller/PixelArtController.java
+++ PixelArt/src/main/java/controller/PixelArtController.java
@@ -1,6 +1,14 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+
 public class PixelArtController {
+    @FXML
+    private Canvas canvasPixelArt;
+    @FXML
+    private Button buttonGenerateCode;
     public void initialize(){
 
     }
